f295e0aebb0f782bd750ec0394facd40601521a4
devflamenbaum <bielflam.engpet@gmail.com>
2024-08-28 19:52:07 -0300

    Creating app: initial deps setup

---
+++ app/assessment-pismo/src/main/java/dev/flamenbaum/assessment/AssessmentPismoApplication.java
@@ -0,0 +1,13 @@
+package dev.flamenbaum.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentPismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentPismoApplication.class, args);
+	}
+
+}
