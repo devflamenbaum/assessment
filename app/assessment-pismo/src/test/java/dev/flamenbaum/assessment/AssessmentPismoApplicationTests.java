c054e830b21a0722b696593adf30eae482a868fc
devflamenbaum <68926842+devflamenbaum@users.noreply.github.com>
2024-08-28 20:09:52 -0300

    App: Initial Configurations (#2)
    * Creating app: initial deps setup
    
    * Database Configuration in application.yaml

---
+++ app/assessment-pismo/src/test/java/dev/flamenbaum/assessment/AssessmentPismoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flamenbaum.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentPismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
